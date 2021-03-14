c107b73995070f26d803cbd6a83b713e6d26d99f
NguyenTuanHung14 <17110156@student.hcmute.edu.vn>
2021-03-14 17:15:14 +0700

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'countries', component: CountriesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
